78a155533d5ae6d712d4e431a8e7c6e6b6af2e4d
vennylewis <44182446+vennylewis@users.noreply.github.com>
2019-06-12 21:57:25 +0800

    chore: restructure project

---
+++ src/components/common/Input.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Text, View, TextInput } from 'react-native';
+
+const Input = ({ label, value, onChangeText, placeholder, secureTextEntry }) => {
+    const { inputStyle, labelStyle, containerStyle } = styles;
+
+    return (
+        <View style={containerStyle}>
+            <Text style={labelStyle}>{label}</Text>
+            <TextInput 
+                secureTextEntry={secureTextEntry}
+                placeholder={placeholder}
+                autoCorrect={false}
+                style={inputStyle}
+                value={value}
+                onChangeText={onChangeText}
+            />
+        </View>
+    );
+};
+
+const styles = {
+    //flex value is like proportion of space it is given, like input is 2/3 of space
+    inputStyle: {
+        color: '#000',
+        paddingRight: 5,
+        paddingLeft: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        flex: 2
+    },
+    labelStyle: {
+        fontSize: 18,
+        paddingLeft: 20,
+        flex: 1
+    },
+    containerStyle: {
+        height: 40,
+        flex: 1,
+        flexDirection: 'row',
+        alignItems: 'center'
+    }
+};
+
+export { Input };
