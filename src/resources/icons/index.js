d6469a784ba8a62210bf59f73458ceb2129c011d
vennylewis <44182446+vennylewis@users.noreply.github.com>
2019-06-14 22:36:50 +0800

    chore: add icons for status

--- src/resources/icons/index.js
+++ src/resources/icons/index.js
@@ -1,2 +1,6 @@
 export const Open = require('./open.png');
+export const Close = require('./close.png');
+export const Paid = require('./paid.png');
+export const Arrive = require('./arrive.png');
+export const Complete = require('./complete.png');
 export const Placeholder = require('./placeholder.png');
