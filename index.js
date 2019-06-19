d0c89372268bbe5e10c5a033c02f56b4263d054a
vennylewis <44182446+vennylewis@users.noreply.github.com>
2019-06-19 15:46:11 +0800

    feat: create jiojoinerorder skeleton

--- index.js
+++ index.js
@@ -3,19 +3,14 @@
  */
 
 // import {AppRegistry} from 'react-native';
-// import JioInformation from './src/screens/JioInformation';
+// import JioJoinerOrder from './src/screens/JioJoinerOrder';
 // // import {name as appName} from './app.json';
 
-// AppRegistry.registerComponent('kampongjio', () => JioInformation);
+// AppRegistry.registerComponent('kampongjio', () => JioJoinerOrder);
 
 
 import {AppRegistry} from 'react-native';
 import App from './src/App';
-import JioInfo from './src/components/JioInformation/JioInfo';
 // import {name as appName} from './app.json';
 
-<<<<<<< HEAD
 AppRegistry.registerComponent('kampongjio', () => App);
-=======
-AppRegistry.registerComponent('kampongjio', () => MainPage);
->>>>>>> master
