246129b905d747fd9bd3ffa9a05aa8b25066a3a7
vennylewis <44182446+vennylewis@users.noreply.github.com>
2019-06-24 11:58:09 +0800

    chore

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
-// // AppRegistry.registerComponent('kampongjio', () => JioJoinerOrder);
+AppRegistry.registerComponent('kampongjio', () => App);
 
 // import {AppRegistry} from 'react-native';
 // import Coordinator from './src/screens/Coordinator';
