1d7199fdf69054d8cec1054de4131e6233e7e0ae
vennylewis <44182446+vennylewis@users.noreply.github.com>
2019-06-26 00:52:49 +0800

    able to map from local json file

--- src/components/MainPage/JioList.js
+++ src/components/MainPage/JioList.js
@@ -2,58 +2,58 @@
 // into individual JioDetails. Once we hook up to more data, we need to consider
 // changing from ScrollView to SectionList (the latter only renders what is seen
 // in the screen)
+// A good resource for fetching local json file and rendering it (in a mapping way)
+// https://www.youtube.com/watch?v=5vFgqCfggC0
 
 import React, { Component } from 'react';
 import { ScrollView, Text } from 'react-native';
 import JioDetails from './JioDetails';
-import { db } from '../../config';
+// import { db } from '../../config';
 import data from '../../data/AllJios.json';
 
 class JioList extends Component {
-    state = { allOrders: {}, orderOne: {}, orderTwo: {}, orderThree: {} };
-
-    componentDidMount() { // this adds ALL ORDERS + details eg. this.state.allOrders.order1.store should open "Makisan"
-        db
-            .ref('/allOrders')
-            .on('value', snapshot => {
-                let allOrders = snapshot.val();
-                this.setState({ allOrders });
-                this.setState({ orderOne: allOrders.order1 });
-                this.setState({ orderTwo: allOrders.order2 });
-                this.setState({ orderThree: allOrders.order3 });
-                console.log(allOrders);
-            });
-        // fetch(data)
-        //     .then((response) => response.json())
-        //     .then((responseData) => this.setState({ allOrders: data}))
-    }
+    state = { allOrders: data.allOrders, orderOne: {}, orderTwo: {}, orderThree: {} };
+    
+    // componentDidMount() { // this adds ALL ORDERS + details eg. this.state.allOrders.order1.store should open "Makisan"
+    //     // db
+    //     //     .ref('/allOrders')
+    //     //     .on('value', snapshot => {
+    //     //         let allOrders = snapshot.val();
+    //     //         this.setState({ allOrders });
+    //     //         this.setState({ orderOne: allOrders.order1 });
+    //     //         this.setState({ orderTwo: allOrders.order2 });
+    //     //         this.setState({ orderThree: allOrders.order3 });
+    //     //         console.log(allOrders);
+    //     //     });
+    //     // fetch(data)
+    //     //     .then((response) => response.json())
+    //     //     .then((responseData) => this.setState({ allOrders: data}))
+    //     this.setState({allOrders:order})
+    // }
 
     renderJio() {
         // console.log(this.state.allOrders.order1.coordinaterName);
         // console.log(this.state.check);
-        return (
-             <JioDetails print={'print'} order={this.state.orderOne}/>
-            // A new method doesn't allow the rendering of two components {/* <JioDetails print={'print'} order={this.state.allOrders}/> */}
-            );
-            // return this.state.albums.map(album => 
-            //     <AlbumDetail key={album.title} album={album}/>
+        return this.state.allOrders.map(order => 
+            <JioDetails order={order}/>
+        );
+            //  <JioDetails print={'print'} order={this.state.orderOne}/>
+            // // A new method doesn't allow the rendering of two components {/* <JioDetails print={'print'} order={this.state.allOrders}/> */}
             // );
-            
     }
 
     render() {
         return(
             <ScrollView>
-                {/* { this.renderJio() }  */}
-                <JioDetails order={this.state.orderOne} />
+                { this.renderJio() } 
+                {/* <JioDetails order={this.state.orderOne} />
                 <JioDetails order={this.state.orderTwo} />
                 <JioDetails order={this.state.orderThree} />
-                <JioDetails order={this.state.allOrders} /> 
+                <JioDetails order={this.state.allOrders} />  */}
                 {/* <Text>{this.state.allOrders.store}</Text> */}
             </ScrollView>
         );
     }
 }
-
 
 export default JioList;
