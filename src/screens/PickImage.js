de64c40c19d5bfabd7a2c56629b4a9f5e44d80fc
ngyoshi275@gmail.com <ngyoshi275@gmail.com>
2019-07-24 14:48:25 +0800

    attempt 1 at image to firebase

--- src/screens/PickImage.js
+++ src/screens/PickImage.js
@@ -3,10 +3,13 @@ import { View, Text, TouchableOpacity, Image } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { Logo } from '../resources/images';
 import ImagePicker from 'react-native-image-picker'
+import RNFetchBlob from 'react-native-fetch-blob'
+import { storage } from '../config'
 
 class PickImage extends Component {
     state = {
-        avatarSource: null
+        avatarSource: null,
+        uri: null
     }
 
     pickImage() {
@@ -28,22 +31,40 @@ class PickImage extends Component {
             console.log('Response = ', response);
         
             if (response.didCancel) {
-            console.log('User cancelled image picker');
+                console.log('User cancelled image picker');
             } else if (response.error) {
-            console.log('ImagePicker Error: ', response.error);
+                console.log('ImagePicker Error: ', response.error);
             } else if (response.customButton) {
-            console.log('User tapped custom button: ', response.customButton);
+                console.log('User tapped custom button: ', response.customButton);
             } else {
-            const source = { uri: response.uri };
-        
-            // You can also display the image using data:
-            // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-        
-            this.setState({
-                avatarSource: source,
-            }, () => {
-                console.log('IMAGE LOADED UP!!!')
-            });
+                const source = { uri: response.uri };
+                this.setState({
+                    avatarSource: source,
+                    uri: source.uri
+                }, () => {
+                    // const uploadUri = Platform.OS === 'ios' ? uri.replace('file://', '') : uri
+                    console.log('IMAGE LOADED UP!!!')
+                    const Blob = RNFetchBlob.polyfill.Blob
+                    const fs = RNFetchBlob.fs
+                    windowXMLHttpRequest = RNFetchBlob.polyfill.XMLHttepRequest
+                    window.Blob = Blob
+                    let uploadBlob = null
+                    const imageRef = storage.ref('images').child('hello.jpg')
+                    fs
+                        .readFile(this.state.avatarSource.uri, 'base64')
+                        .then((data) => {
+                            return Blob.build(data, { type: `${mime};BASE64`})
+                        })
+                        .then((blob) => {
+                            uploadBlob = blob
+                            return imageRef.put(blob, { contentType: mime })
+                        })
+                        .then(() => {
+                            uploadBlob.close()
+                            return imageRef.getDownloadURL()
+                            console.log(imageRef.getDownloadURL())
+                        })
+                });
             }
         });
     }
